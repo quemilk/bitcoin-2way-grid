c7991d06cf7ea38c4ca23a7fd357cdd4309111e0
jie <how@how.how>
2021-05-24 13:44:08 +0800

    1

---
+++ src/global.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//实盘API交易地址如下：
+
+#define REST_API_HOST               "https://www.okex.com"
+#define WS_PUBLIC_CHANNEL           "wss://ws.okex.com:8443/ws/v5/public"
+#define WS_PRIVATE_CHANNEL          "wss://ws.okex.com:8443/ws/v5/private"
+
+// AWS 地址如下：
+#define AWS_REST_API_HOST           "https://aws.okex.com"
+#define AWS_WS_PUBLIC_CHANNEL       "wss://wsaws.okex.com:8443/ws/v5/public"
+#define AWS_WS_PRIVATE_CHANNEL      "wss://wsaws.okex.com:8443/ws/v5/private"
+
+// 模拟盘API交易地址如下：
+#define SIMU_REST_API_HOST          "https://www.okex.com"
+#define SIMU_WS_PUBLIC_CHANNEL      "wss://wspap.okex.com:8443/ws/v5/public?brokerId=9999"
+#define SIMU_WS_PRIVATE_CHANNEL     "wss://wspap.okex.com:8443/ws/v5/private?brokerId=9999"
